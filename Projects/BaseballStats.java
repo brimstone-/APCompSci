9ee1e3018a5e5385cd00609ce13b487327e163f0
Matthew Cheng <e123matt@gmail.com>
2014-09-06 09:56:05 -0500

    Continued work on BaseballStats

--- Projects/BaseballStats.java
+++ Projects/BaseballStats.java
@@ -4,8 +4,37 @@
  * @author Matthew Cheng
  * @version  9/6
  */
+
+// ****************************************************************
+//   BaseballStats.java
+//
+//   Reads baseball data in from a comma delimited file. Each line
+//   of the file contains a name followed by a list of symbols
+//   indicating the result of each at bat: h for hit, o for out,
+//   w for walk, s for sacrifice.  Statistics are computed and
+//   printed for each player.
+// ****************************************************************
+import java.util.Scanner;
+import java.io.*;
 public class BaseballStats {
-	public static void main (String [] args) {
-		
-	}
+    //-------------------------------------------------
+    //  Reads baseball stats from a file and counts
+    //  total hits, outs, walks, and sacrifice flies
+    //  for each player.
+    //-------------------------------------------------
+    public static void main (String[] args) throws IOException {
+        Scanner fileScan, lineScan;
+        String fileName, player;
+        Scanner scan = new Scanner(System.in);
+        System.out.print ("Enter the name of the input file: ");
+
+        fileName = scan.nextLine();
+        File file = new File(fileName);
+        fileScan = new Scanner(new File(fileName));
+        
+        while (fileScan.hasNextLine()) {
+        	player = scan.nextLine();
+        	System.out.println(player);
+        }
+    }
 }
