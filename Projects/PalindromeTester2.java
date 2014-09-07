58b1a10a6495d699158d81a84a2d5d19b21106cd
Matthew Cheng <e123matt@gmail.com>
2014-09-06 23:53:39 -0500

    PalindromeTester2 complete with far less use of regex. However isLetter(); was still used. This seems to have avoided a long stupid list of checks for each individual symbol/punctuation or conversely checks for each letter of the alphabet in both cases.

--- Projects/PalindromeTester2.java
+++ Projects/PalindromeTester2.java
@@ -8,19 +8,69 @@
 import java.util.Scanner;
 
 public class PalindromeTester2 {
+
     public static void main (String [] args) {
-        String input, reverse, another = "y";
+
+        String input, reverse = "", another = "y";
         Scanner sc = new Scanner (System.in);
 
         while (another.equalsIgnoreCase("y")) {
             System.out.print("Please enter a potential palindrome: ");
             input = sc.nextLine();
 
-           	
+            reverse = reverseString(input);
+            //System.out.println();
+            //System.out.println(reverse);
+            reverse = letterOnly(reverse);
+            //System.out.println();
+            //System.out.println(reverse);
+            input = letterOnly(input);
+            //System.out.println();
+            //System.out.println(input);
+
+            boolean equals = input.equalsIgnoreCase(reverse);
+
+            if (equals) {
+                System.out.println();
+                System.out.println("That string IS a palindrome.");
+            } else {
+                System.out.println();
+                System.out.println("That string IS NOT a palindrome.");
+            }
 
             System.out.println();
             System.out.print("Test another palindrome (y/n)? ");
             another = sc.nextLine();
         }
+    }
+
+    public static String reverseString(String Line) {
+
+        int length = Line.length() - 1;
+
+        String s = "";
+
+        for (int i = length; i >= 0; i--) {
+            if (Line.charAt(i) != ' ') {
+                s += Line.charAt(i);
+            }
+        }
+
+        return s;
+    }
+
+    public static String letterOnly(String Line) {
+
+        int length = Line.length();
+
+        String s = "";
+
+        for (int i = 0; i < length; i++) {
+            if (Character.isLetter(Line.charAt(i))) {
+                s += Line.charAt(i);
+            }
+        }
+        
+        return s;
     }
 }
