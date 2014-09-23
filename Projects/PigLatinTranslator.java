8ea786b4997913ce6a8a70901ecc1ebf8f417201
Matthew Cheng <mcheng5@students.d125.org>
2014-09-23 14:54:59 -0500

    Update PigLatinTranslator.java

--- Projects/PigLatinTranslator.java
+++ Projects/PigLatinTranslator.java
@@ -56,20 +56,20 @@ public class PigLatinTranslator
     private boolean beginsWithBlend (String word)
     {
         return ( word.startsWith ("bl") || word.startsWith ("sc") ||
-            word.startsWith ("br") || word.startsWith ("sh") ||
-            word.startsWith ("ch") || word.startsWith ("sk") ||
-            word.startsWith ("cl") || word.startsWith ("sl") ||
-            word.startsWith ("cr") || word.startsWith ("sn") ||
-            word.startsWith ("dr") || word.startsWith ("sm") ||
-            word.startsWith ("dw") || word.startsWith ("sp") ||
-            word.startsWith ("fl") || word.startsWith ("sq") ||
-            word.startsWith ("fr") || word.startsWith ("st") ||
-            word.startsWith ("gl") || word.startsWith ("sw") ||
-            word.startsWith ("gr") || word.startsWith ("th") ||
-            word.startsWith ("kl") || word.startsWith ("tr") ||
-            word.startsWith ("ph") || word.startsWith ("tw") ||
-            word.startsWith ("pl") || word.startsWith ("wh") ||
-            word.startsWith ("pr") || word.startsWith ("wr") );
+                 word.startsWith ("br") || word.startsWith ("sh") ||
+                 word.startsWith ("ch") || word.startsWith ("sk") ||
+                 word.startsWith ("cl") || word.startsWith ("sl") ||
+                 word.startsWith ("cr") || word.startsWith ("sn") ||
+                 word.startsWith ("dr") || word.startsWith ("sm") ||
+                 word.startsWith ("dw") || word.startsWith ("sp") ||
+                 word.startsWith ("fl") || word.startsWith ("sq") ||
+                 word.startsWith ("fr") || word.startsWith ("st") ||
+                 word.startsWith ("gl") || word.startsWith ("sw") ||
+                 word.startsWith ("gr") || word.startsWith ("th") ||
+                 word.startsWith ("kl") || word.startsWith ("tr") ||
+                 word.startsWith ("ph") || word.startsWith ("tw") ||
+                 word.startsWith ("pl") || word.startsWith ("wh") ||
+                 word.startsWith ("pr") || word.startsWith ("wr") );
     }
 }
 
