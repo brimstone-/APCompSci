745dd53069a4ec62d0d040286124808c15cad675
Matthew Cheng <mcheng5@students.d125.org>
2014-09-30 16:53:17 -0500

    Update Phone.java

--- Projects/Phone.java
+++ Projects/Phone.java
@@ -9,12 +9,12 @@ public class Phone
     private String brandName = "UNKNOWN", modelType = "UNKNOWN", ownerName = "UNKNOWN";
     private int batteryLife = 0;
     private double spaceLeft = 0;
-    
+
     public Phone()
     {
-        
+
     }
-    
+
     public Phone(String owner, String brand, String model, int battery, double space)
     {
         brandName = brand;
@@ -24,6 +24,11 @@ public class Phone
         spaceLeft = space;
     }
 
+    public String makeCall(String number)
+    {
+        return "You dial " + number + " but it seems that you have no provider, so you can't make calls.";
+    }
+    
     public void setPhone(String owner, String brand, String model, int battery, double space)
     {
         brandName = brand;
@@ -32,52 +37,52 @@ public class Phone
         batteryLife = battery;
         spaceLeft = space;
     }
-    
+
     public String getBrand()
     {
         return brandName;
     }
-    
+
     public void setBrand(String brand)
     {
         brandName = brand;
     }
-    
+
     public String getModel()
     {
         return modelType;
     }
-    
+
     public void setModel(String model)
     {
         modelType = model;
     }
-    
+
     public String getOwner()
     {
         return ownerName;
     }
-    
+
     public void setOwner(String owner)
     {
         ownerName = owner;
     }
-        
+
     public int getBatteryLife()
     {
         return batteryLife;
     }
-    
+
     public void setBatteryLife(int battery)
     {
         batteryLife = battery;
     }
-    
+
     public double getSpace()
     {
         return spaceLeft;
     }
-    
+
     public void setSpace(double space)
     {
         spaceLeft = space;
@@ -85,7 +90,7 @@ public class Phone
 
     public String toString()
     {
-        return "This phone belongs to" + ownerName + " and is a " + modelType + " made by " + brandName + " it has " + batteryLife + "% battery left and has " + spaceLeft + " Gb of storage space left.";
+        return "This phone belongs to " + ownerName + " and is a " + modelType + " made by " + brandName + " it has " + batteryLife + "% battery left and has " + spaceLeft + " Gb of storage space left.";
     }
 
 }
