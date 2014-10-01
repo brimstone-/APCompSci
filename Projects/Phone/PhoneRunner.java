6d23ea4760f74b249378f408597c756c9de1f557
Matthew Cheng <e123matt@gmail.com>
2014-10-01 00:07:20 -0500

    Updated binaries

--- Projects/Phone/PhoneRunner.java
+++ Projects/Phone/PhoneRunner.java
@@ -9,6 +9,11 @@ public class PhoneRunner {
     	Phone p1 = new Phone("Matthew","Apple","iPhone 5",99,1.3);
     	System.out.println(p1+"\n");
     	p1.setModel("iPhone 6");
-    	System.out.println(p1);
+    	p1.setSpace(30.2);
+    	System.out.println("Matthew has upgraded his phone.\n\n" + p1);
+    	p1.setBrand("Google");
+    	p1.setModel("Nexus 5");
+    	p1.setSpace(60);
+    	System.out.println("\nMatthew comes to his senses and actually upgrades to a real phone this time.\n\n" + p1);
     }
 }
