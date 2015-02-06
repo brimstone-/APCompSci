7ead662630b7f341b8c98ffe18fe8e9e15b45d76
Matt <e123matt@gmail.com>
2015-02-05 22:30:18 -0600

    Fix comment

--- Projects/7.3 Expense Planner/ExpensePlanner.java
+++ Projects/7.3 Expense Planner/ExpensePlanner.java
@@ -1,7 +1,7 @@
 public class ExpensePlanner
 {
     //----------------------------------------------
-    // Creates the displays the vote counter GUI.
+    // Creates and displays the vote counter GUI.
     //----------------------------------------------
     public static void main(String[] args)
     {
