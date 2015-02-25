5e50b3e907d0ac3d37854a8fcc36204c70324879
Matthew Cheng <mcheng5@students.d125.org>
2015-02-25 15:22:40 -0600

    Update ExpressionTree.java

--- Projects/8.1 ExpressionTree Project/ExpressionTree.java
+++ Projects/8.1 ExpressionTree Project/ExpressionTree.java
@@ -29,26 +29,28 @@ public class ExpressionTree
         int multiply = s.lastIndexOf("*");
         int divide = s.lastIndexOf("/");
         int mod = s.lastIndexOf("%");
-        
+        ExpressionNode result;
         if (plus > -1 || minus > -1) {
             if (plus > minus) {
-                return new ExpressionNode("+", build(s.substring(0, plus)), build(s.substring(plus+1)));
+                result = new ExpressionNode("+", build(s.substring(0, plus)), build(s.substring(plus+1)));
             }
             else {
-                return new ExpressionNode("-", build(s.substring(0, minus)), build(s.substring(minus+1)));
+                result = new ExpressionNode("-", build(s.substring(0, minus)), build(s.substring(minus+1)));
             }
         }
         else if (multiply > -1 || divide > -1 || mod > -1) {
             if (multiply > divide && multiply > mod) {
-                return new ExpressionNode("*", build(s.substring(0, multiply)), build(s.substring(multiply+1)));
+                result = new ExpressionNode("*", build(s.substring(0, multiply)), build(s.substring(multiply+1)));
             }
             else if (divide > multiply && divide > mod) {
-                return new ExpressionNode("/", build(s.substring(0, divide)), build(s.substring(divide+1)));
+                result = new ExpressionNode("/", build(s.substring(0, divide)), build(s.substring(divide+1)));
             }
             else
-                return new ExpressionNode("%", build(s.substring(0, mod)), build(s.substring(mod+1)));
+                result = new ExpressionNode("%", build(s.substring(0, mod)), build(s.substring(mod+1)));
         }
-        else
-            return new ExpressionNode(s, null, null);
+        else {
+            result = new ExpressionNode(s, null, null);
+        }
+        return result;
     }
 }
