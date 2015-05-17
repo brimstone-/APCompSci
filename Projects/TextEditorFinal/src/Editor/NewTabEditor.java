8494b702bab198d55755068c83dafa2468d967bc
Matt <e123matt@gmail.com>
2015-05-17 01:11:36 -0500

    Updated the way opening a file works so that it makes more sense.  Also added some more documentation

--- Projects/TextEditorFinal/src/Editor/NewTabEditor.java
+++ Projects/TextEditorFinal/src/Editor/NewTabEditor.java
@@ -8,8 +8,11 @@ import javax.swing.event.*;
 import java.awt.event.*;
 import java.io.*;
 
+// These libraries are for the better and stronger versions of JTextArea and JScrollPane
 import org.fife.ui.rtextarea.*;
 import org.fife.ui.rsyntaxtextarea.*;
+
+// This is for better saving to file
 import org.apache.commons.io.*;
 
 class NewTabEditor implements Runnable { // Trying again, this time with tabs
@@ -59,7 +62,9 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 	}
 
 	public void run() {
-		listener = new ChangeListener() {  // ChangeListener for the adding tabs button
+		listener = new ChangeListener() {
+			// ChangeListener for the adding tabs button
+			// This listener has been both really helpful to me and really annoying
 			public void stateChanged(ChangeEvent e) {
 				addNewTab();
 			}
@@ -83,6 +88,7 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 															// closing tabs with middle mouse click
 
 		// Set mnemonics/accelerators for keyboard shortcuts, activated with CTRL+KEY
+		// These are a lot of fun
 		file.setMnemonic(KeyEvent.VK_F);
 		openButton.setMnemonic(KeyEvent.VK_O);
 		openButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
@@ -104,7 +110,7 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 		openButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-					readFile(fileChooser.getSelectedFile().getAbsolutePath());
+					readFile(fileChooser.getSelectedFile().getAbsolutePath(), fileChooser.getSelectedFile().getName());
 					// Use the file reader to get the selected file,
 					// and then find it's full directory path.
 					// This is then given to the readFile method.
@@ -112,7 +118,14 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 			}
 		});
 
-		newButton.addActionListener(new ActionListener () {
+		newButton.addActionListener(new ActionListener () { // This method is a bit ugly, but long story short, it adds a new tab from scratch
+															// I wanted to make a method that adds tabs, but as it turns out, it only got used once
+															// since the utility of a super generic tab only goes so far
+															// In most cases, every tab I make has something different about it
+															// the if (numTabs > 1) else structure is to fix the bug that occurs
+															// when the user makes a tab and the "+" is selected, or no tab at all
+															// or worse, ruining the functionality of the "+" button entirely
+															// I also had trouble with double tab creation
 			public void actionPerformed(ActionEvent e) {
 				if (numTabs > 1) {
 					int index = tabs.getSelectedIndex();
@@ -199,7 +212,7 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 
 		toggle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				SwingUtilities.invokeLater(new Runnable() { // Here I update the GUI
+				SwingUtilities.invokeLater(new Runnable() {// Here I update the GUI, thanks invokeLater!
 					@Override // I underestimated the use of @Override until now
 					public void run() {
 						RSyntaxTextArea text = getText(tabs.getSelectedIndex());
@@ -243,6 +256,7 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 
 		frame.setJMenuBar(menuBar);
 
+		// Good ol' GUI staples, setting behaviors, etc.
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(tabs, BorderLayout.CENTER);
 
@@ -251,25 +265,6 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 
-	}
-
-	private void addTab() {
-		int index = numTabs - 2;
-		JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-
-		RSyntaxTextArea text = new RSyntaxTextArea();
-		text.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
-		text.setFont(monospaced);
-
-		RTextScrollPane scroll = new RTextScrollPane(text);
-
-		panel.add(scroll, BorderLayout.CENTER);
-		panel.setBorder(BorderFactory.createEmptyBorder());
-
-		tabs.add(panel, "Untitled", index);
-
-		numTabs++;
 	}
 
 	private void addNewTab() {
@@ -291,7 +286,7 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 			tabs.add(panel, "Untitled", index);
 			tabs.setMnemonicAt(index, index);
 
-			SwingUtilities.invokeLater(new Runnable() { // Here I update the GUI
+			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
 					tabs.setSelectedIndex(numTabs - 2);
@@ -301,11 +296,14 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 		}
 	}
 
-	private void readFile(String fileName) {
+	private void readFile(String filePath, String fileName) {
 		try {
 			int index = tabs.getSelectedIndex();
 
-			FileReader r = new FileReader(fileName); // Create a FileReader
+			FileReader r = new FileReader(filePath); // Create a FileReader
+			
+			// I was considering using commons-IO here too in order to read in files,
+			// but strings have maximum sizes and I don't want to deal with that
 
 			RSyntaxTextArea text = new RSyntaxTextArea(); // New TextArea
 
@@ -322,36 +320,37 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 
 			panel.add(scroll, BorderLayout.CENTER); // This allows the ScrollPanel/TextArea to size with the Panel
 			panel.setBorder(BorderFactory.createEmptyBorder());
-
-			tabs.add(panel, fileName, index); // Add the Panel to the TabbedPanel
-			tabs.setMnemonicAt(index, index); // Set up keyboard shortcut for any new tab
-
-			tabs.remove(index + 1);
-			tabs.remove(index + 1);
-
-			SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					tabs.setSelectedIndex(index);
-				}
-			});
-			numTabs--;
-
+			
+			if (numTabs < 2) {  // Weird bug where if user tries to open file when only the "+" exists, things break.
+								// This avoids those shenanigans, and gives me an excuse to use beep() again.
+				Toolkit.getDefaultToolkit().beep(); // If no text tabs exist, yell at the user
+				JOptionPane.showMessageDialog(tabs, "You need at least one tab open to open a file");
+			}
+			else {
+				tabs.add(panel, fileName, index+1); // Add the Panel to the TabbedPanel
+				numTabs++;
+			
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						tabs.setSelectedIndex(index+1);
+					}
+				});
+			}
 		} catch (IOException e) {
-			Toolkit.getDefaultToolkit().beep(); // If the file didn't exist, yell at the user
+			Toolkit.getDefaultToolkit().beep(); // If the file didn't exist, yell at the user... beep!
 			JOptionPane.showMessageDialog(tabs, "Editor can't find the file called " + fileName);
 		}
 	}
 
-	private void saveFile(String fileName) {
+	private void saveFile(String fileName) { // Save is really just a save as, overwriting each time
 		try {
 			int index = tabs.getSelectedIndex();
-
 
 			RSyntaxTextArea text = getText(tabs.getSelectedIndex());
 
 			String save = text.getText();
-			FileUtils.writeStringToFile(new File(fileName), save);    // However, commons-io is totally awesome
+			FileUtils.writeStringToFile(new File(fileName), save);    // However, commons-IO is totally awesome
 
 			tabs.setTitleAt(index, fileName);
 		} catch (IOException e) {
@@ -371,10 +370,10 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 
 	private RSyntaxTextArea getText(int index) {
 		Container panel = (Container)tabs.getComponentAt(index);    // This container junk along with
-		Container scroll = (Container)panel.getComponents()[0];     // awt creating viewports for things inside
-		Container port = (Container)(scroll.getComponents()[0]);    // of scrollpanes took so long to figure out
+		Container scroll = (Container)panel.getComponents()[0];     // AWT creating Viewport s for things inside
+		Container port = (Container)(scroll.getComponents()[0]);    // of RTextScrollPane s took so long to figure out
 																	// it is seriously not funny
-																	// I had to keep incorrectly casting things
+		// Hooray for casting though								// I had to keep incorrectly casting things
 																	// so the error messages would tell me these things'
 																	// "True Identity", awful experience
 		JViewport realPort = (JViewport)port;
@@ -387,8 +386,9 @@ class NewTabEditor implements Runnable { // Trying again, this time with tabs
 	public class MiddleClickListener extends MouseAdapter { // Listener for the closing tab clicking action
 		public void mouseClicked(MouseEvent e) {
 			if (SwingUtilities.isMiddleMouseButton(e)) {
+				
 				int index = tabs.getSelectedIndex();
-				tabs.remove(tabs.indexAtLocation(e.getX(), e.getY()));
+				tabs.remove(tabs.indexAtLocation(e.getX(), e.getY())); // Get the tab index at the location of the click
 				numTabs--;
 
 				SwingUtilities.invokeLater(new Runnable() { // Here I update the GUI
